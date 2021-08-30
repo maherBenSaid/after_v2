507d5af50b9cc7ea2fbae524431ba81967d8d3d2
Maher BEN SAID <Maher.BENSAID@amadeus.com>
2021-08-30 12:42:46 +0200

    adding pear

---
+++ Fruit/src/com/pluralsight/fruit/Kiwi.java
@@ -0,0 +1,11 @@
+package com.pluralsight.fruit;
+
+public class Kiwi implements IFruit {
+  @Override public String getName() {
+    return null;
+  }
+
+  @Override public int getRollMultiplier() {
+    return 0;
+  }
+}
